b8ca61fbbb5d7522d5d641cc84c276ca82c05662
Jamin <765426599@qq.com>
2025-03-19 13:43:56 +0800

    modify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import { NextConfig } from 'next';
 import { withContentlayer } from 'next-contentlayer2';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY && process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
+  assetPrefix,
+  basePath,
   reactStrictMode: true,
   output: "export"
 };
